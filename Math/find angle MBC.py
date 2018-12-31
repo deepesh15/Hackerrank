1e88d0d64b74dc29bee809743bac5fc478a1ec8c
deepesh15 <39853188+deepesh15@users.noreply.github.com>
2018-12-31 23:45:02 +0530

    Add files via upload

--- Math/find angle MBC.py
+++ Math/find angle MBC.py
@@ -1,7 +1,9 @@
 import math
-
-ab = int(input())
-bc = int(input())
-ac = math.sqrt(ab**2+bc**2)
-
-print(ac)
+y1 = int(input())       #y co-ordiate of AB
+x2 = int(input())       # x co-ordinate of BC
+mx = x2/2               #mid x co-ordinate of hypotenuse AC
+my = y1/2               #mid y co-ordinate of hypotenuse BC
+dis_frm_orign = math.sqrt(mx**2+my**2)
+rad_angle = math.degrees(math.atan(y1/x2))
+angle = str(int(round(rad_angle)))+"°"
+print(angle)
