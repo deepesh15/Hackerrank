491dde55a427635ef170377fb07b373272d72652
deepesh15 <39853188+deepesh15@users.noreply.github.com>
2019-01-02 16:57:37 +0530

    Add files via upload

--- Basic Data Types/Nested Lists.py
+++ Basic Data Types/Nested Lists.py
@@ -1,12 +1,24 @@
+marksheet = []
+for _ in range(0,int(input())):
+    marksheet.append([input(), float(input())])
+
+second_highest = sorted(list(set([marks for name, marks in marksheet])))[1]
+print('\n'.join([a for a,b in sorted(marksheet) if b == second_highest]))
+
+
+
+
+
+''''
 if __name__ == '__main__':
     n_list = []
     for _ in range(0,int(input())):
         n_list.append([input(),float(input())])
     
-    print(n_list)
-    n_list.sort
+    sorted(n_list, key = n_list[0][1])
     print(n_list)
     
     #sprint(student_marks)
 
 
+'''''
