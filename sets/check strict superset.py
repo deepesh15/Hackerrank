597f9004243621c1d28835101dc47620ca2b6a2c
deepesh15 <39853188+deepesh15@users.noreply.github.com>
2019-01-05 20:28:50 +0530

    Add files via upload

--- sets/check strict superset.py
+++ sets/check strict superset.py
@@ -1,8 +1,2 @@
-set_A = set(map(int,input().split()))
-literal = False
-for _ in range(int(input())):
-    set_B = set(map(int,input().split()))
-    if set_A.issubset(set_B) and len(set_B)<len(set_A):
-        literal =True
-    
-print(literal)
+a = set(input().split())
+print(all(a > set(input().split()) for _ in range(int(input()))))
