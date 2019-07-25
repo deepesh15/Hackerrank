e526bf083a784cee8c68ff57145b3b27dc19c9f2
Deepesh Yadav <39853188+deepesh15@users.noreply.github.com>
2019-07-25 17:51:04 +0530

    Add files via upload
    25-07-2019

---
+++ Equalize the array.py
@@ -0,0 +1,26 @@
+'''
+import statistics as st
+n =  int(input())
+temp = list(map(int,input().split()))
+arr= sorted(temp)
+#print(len(arr))
+count = 0
+try:
+    m = st.mode(arr)
+except st.StatisticsError as e:
+    print(n-1)
+else:
+    for i in range(n):
+        if(arr[i] != m):
+            del[i]
+            count += 1
+    print(count)
+    print(m)
+    
+'''
+
+import  collections as col
+
+n = int(input())
+lst = list(map(int,input().split()))
+print(n  - max([lst.count(i) for i in set(lst)]))
